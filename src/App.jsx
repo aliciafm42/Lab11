5d0d0342af1c584ff975c1657172aee9ccb43857
aliciafm42 <156551143+aliciafm42@users.noreply.github.com>
2025-09-04 14:23:51 -0400

    2:23

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Header.jsx';
+import './Header.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <h1>Profile App</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="header">
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#intro">Introduction</a></li>
+          <li><a href="#cards">Cards</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
