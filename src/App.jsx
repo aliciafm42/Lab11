15ad66c2aac57a7edb62d86af2625374e87d1c42
aliciafm42 <156551143+aliciafm42@users.noreply.github.com>
2025-09-06 22:10:50 -0400

    10:10 250906

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './Header.jsx'
 import Introduction from './Introduction.jsx'
 import Card from './Card.jsx'
+import Wrapper from './Wrapper.jsx' // import your new wrapper
 
 function App() {
   return (
@@ -10,14 +11,17 @@ function App() {
       <Header />
       <h1>Profile App</h1>
       <Introduction />
-       <Card
-        title="Willow"
-        description="Willow trees are diverse, graceful, fast-growing deciduous trees or shrubs characterized by simple, typically narrow, serrated leaves, often on slender, flexible branches" 
-      />
-      <Card 
-        title="Evergreen"
-        description="In botany, an evergreen is a plant which has foliage that remains green and functional throughout the year. This contrasts with deciduous plants, which lose their foliage completely during the winter or dry season." 
-      />
+
+      <Wrapper>
+        <Card
+          title="Willow"
+          description="Willow trees are diverse, graceful, fast-growing deciduous trees or shrubs characterized by simple, typically narrow, serrated leaves, often on slender, flexible branches" 
+        />
+        <Card 
+          title="Evergreen"
+          description="In botany, an evergreen is a plant which has foliage that remains green and functional throughout the year. This contrasts with deciduous plants, which lose their foliage completely during the winter or dry season." 
+        />
+      </Wrapper>
     </>
   )
 }
